530036b5df5a3e2c04d6c872eaec0ffc4dbf6e81
Breno RdV <brenordv@users.noreply.github.com>
2025-08-01 22:54:01 -0400

    recreating the media organizer.

--- crates/scene-media-organizer/src/utils/file_status_controller.rs
+++ crates/scene-media-organizer/src/utils/file_status_controller.rs
@@ -9,8 +9,34 @@ impl FileStatusController {
     pub fn new(db_path: String) -> Result<Self> {
         let db = GenericDb::new(db_path)?;
         
-        
+        Self::create_table(&db)?;
         
         Ok(Self { db })
+    }
+    
+    fn create_table(db: &GenericDb) -> Result<()> {
+        let create_table_sql = r#"
+            CREATE TABLE IF NOT EXISTS files (
+                full_path TEXT PRIMARY KEY,
+                file_name TEXT NOT NULL,
+                parent TEXT NOT NULL,
+                target_path TEXT NOT NULL,
+                item_type TEXT NOT NULL,
+                media_type TEXT NOT NULL,
+                status TEXT NOT NULL,
+                is_archive INTEGER NOT NULL,
+                is_main_archive_file INTEGER NOT NULL,
+                attempts INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                year INTEGER,
+                season INTEGER,
+                episode INTEGER,
+                timestamp TEXT NOT NULL
+            )
+        "#;
+
+        db.execute(create_table_sql, &[])?;
+
+        Ok(())
     }
 }
--- crates/scene-media-organizer/src/utils/mod.rs
+++ crates/scene-media-organizer/src/utils/mod.rs
@@ -1,2 +1,2 @@
 pub mod guessit_client;
-mod file_status_controller;
+pub mod file_status_controller;
