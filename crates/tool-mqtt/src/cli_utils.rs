d1925aa482474bb9430e469f1ef01fca410e902e
Breno RdV <brenordv@users.noreply.github.com>
2025-08-23 23:09:02 -0400

    New tool: MQTT

---
+++ crates/tool-mqtt/src/cli_utils.rs
@@ -0,0 +1,171 @@
+use crate::models::{MqttArgs, MqttCommand};
+use crate::string_traits::StringExt;
+use anyhow::Result;
+use clap::{Arg, Command};
+use shared::command_line::cli_builder::CommandExt;
+use shared::constants::general::DASH_LINE;
+
+pub fn print_runtime_info(args: &MqttArgs) {
+    println!("✉️ MQTT v{}", env!("CARGO_PKG_VERSION"));
+    println!("{}", DASH_LINE);
+
+    println!("Host: {}:{}", args.host, args.port);
+
+    let connection_type = if args.is_anonymous() {
+        "Anonymous"
+    } else {
+        "Authenticated"
+    };
+
+    println!("Connection type: {}", connection_type);
+    println!("Topic: {}", args.topic);
+
+    match args.command {
+        MqttCommand::Unknown => {}
+        MqttCommand::Read => {
+            println!("Command: Read");
+        }
+        MqttCommand::Post => {
+            println!("Command: Post");
+            if let Some(msg) = &args.message {
+                println!("Message: {}", msg);
+            }
+        }
+    }
+}
+
+pub fn get_cli_arguments() -> Result<MqttArgs> {
+    let matches = Command::new(env!("CARGO_PKG_NAME"))
+        .add_basic_metadata(
+            env!("CARGO_PKG_VERSION"),
+            env!("CARGO_PKG_DESCRIPTION"),
+            "Cli tool to perform quickly post to or read from a MQTT broker.",
+        )
+        .arg(
+            Arg::new("command")
+                .help("Command to execute.")
+                .num_args(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("host")
+                .long("host")
+                .short('o')
+                .required(true)
+                .help("Host to connect to."),
+        )
+        .arg(
+            Arg::new("port")
+                .long("port")
+                .short('p')
+                .required(false)
+                .default_value("1883")
+                .help("Port to connect to. (Default: 1883)"),
+        )
+        .arg(
+            Arg::new("topic")
+                .long("topic")
+                .short('t')
+                .required(true)
+                .help("Topic to post to or read from."),
+        )
+        .arg(
+            Arg::new("message")
+                .long("message")
+                .short('m')
+                .required(false)
+                .help("Message to post to the topic."),
+        )
+        .arg(
+            Arg::new("username")
+                .long("username")
+                .short('u')
+                .required(false)
+                .help("Username to connect with."),
+        )
+        .arg(
+            Arg::new("password")
+                .long("password")
+                .short('a')
+                .required(false)
+                .help("Password for authentication."),
+        )
+        .get_matches();
+
+    let command = match matches.get_one::<String>("command") {
+        None => {
+            // Field is required. This shouldn't happen.
+            MqttCommand::Unknown
+        }
+        Some(cmd) => cmd.to_mqtt_command(),
+    };
+
+    let host = matches.get_one::<String>("host").unwrap();
+    let port = matches.get_one::<String>("port").unwrap().parse::<u16>()?;
+    let topic = matches.get_one::<String>("topic").unwrap().trim().to_string();
+    let message = matches.get_one::<String>("message");
+    let username = matches.get_one::<String>("username");
+    let password = matches.get_one::<String>("password");
+
+    Ok(MqttArgs {
+        command,
+        host: host.clone(),
+        port,
+        topic: topic.clone(),
+        message: message.cloned(),
+        username: username.cloned(),
+        password: password.cloned(),
+    })
+}
+
+pub fn validate_args(args: &MqttArgs) -> Result<()> {
+    match args.command {
+        MqttCommand::Unknown => {
+            anyhow::bail!("Unknown command. Review your command line and try again.");
+        }
+        MqttCommand::Read => {}
+        MqttCommand::Post => {
+            if args.message.is_none() {
+                anyhow::bail!("Message is required for post command.");
+            }
+        }
+    }
+
+    validate_host_and_port(&args.host, args.port)?;
+    validate_user_and_password(&args.username, &args.password)?;
+    validate_topic(&args.topic)?;
+
+    Ok(())
+}
+
+fn validate_host_and_port(host: &String, port: u16) -> Result<()> {
+    if host.is_empty() {
+        anyhow::bail!("Host is required.");
+    }
+
+    if port == 0 {
+        anyhow::bail!("Port is required.");
+    }
+
+    Ok(())
+}
+
+fn validate_topic(topic: &String) -> Result<()> {
+    if topic.is_empty() {
+        anyhow::bail!("Topic is required.");
+    }
+
+    Ok(())
+}
+
+fn validate_user_and_password(username: &Option<String>, password: &Option<String>) -> Result<()> {
+    if username.is_none() && password.is_none() {
+        return Ok(());
+    };
+
+    if username.is_some() && password.is_some() {
+        return Ok(());
+    };
+
+    anyhow::bail!("Username and password are required together.");
+}
---
+++ crates/tool-mqtt/src/mqtt_app.rs
@@ -0,0 +1,133 @@
+use crate::models::MqttArgs;
+use anyhow::Result;
+use rumqttc::{AsyncClient, Event, EventLoop, Incoming, MqttOptions, Outgoing, QoS};
+use std::time::Duration;
+use tokio::time::sleep;
+use tracing::{debug, error, info};
+
+pub async fn read_messages(args: &MqttArgs) -> Result<()> {
+    let (topic, client, mut event_loop) = create_connection_options("reader".to_string(), args);
+
+    info!("Subscribing to topic: {}", topic);
+    client.subscribe(&topic, QoS::AtMostOnce).await?;
+    info!("Subscribed to topic: {}", topic);
+
+    info!("Waiting for messages...");
+    loop {
+        match event_loop.poll().await {
+            Ok(event) => match event {
+                Event::Incoming(inc_message) => {
+                    match inc_message {
+                        Incoming::Publish(message) => {
+                            debug!("Publish received: {:?}", message);
+                            let decoded_payload = String::from_utf8(message.payload.to_vec())?;
+                            info!("Message received: {:?}", decoded_payload);
+                        }
+                        _ => {}
+                    }
+                }
+                Event::Outgoing(_) => {
+                    
+                }
+            },
+            Err(e) => {
+                error!("Error = {:?}", e);
+            }
+        }
+
+        sleep(Duration::from_millis(200)).await;
+    }
+}
+
+fn create_connection_options(client_id: String, args: &MqttArgs) -> (String, AsyncClient, EventLoop) {
+    debug!("Creating connection options");
+    let topic = args.topic.clone();
+    let host = args.host.clone();
+    let port = args.port;
+
+    let mut mqtt_options =
+        MqttOptions::new(format!("{}-{}", env!("CARGO_PKG_NAME"), client_id), host, port);
+
+    mqtt_options.set_keep_alive(Duration::from_secs(5));
+
+    if !args.is_anonymous() {
+        let username = args.username.clone().unwrap();
+        let password = args.password.clone().unwrap();
+        mqtt_options.set_credentials(username, password);
+    }
+
+    debug!("Creating connection");
+    let (client, event_loop) = AsyncClient::new(mqtt_options, 10);
+
+    debug!("Connecting to broker");
+    (topic, client, event_loop)
+}
+
+pub async fn post_message(args: &MqttArgs) -> Result<()> {
+    let message = match &args.message {
+        None => {
+            anyhow::bail!("No message provided");
+        }
+        Some(msg) => msg,
+    };
+
+    let (topic, client, mut event_loop) = create_connection_options("sender".to_string(), args);
+
+    info!("Publishing message to topic: {}", topic);
+    client
+        .publish(topic, QoS::AtLeastOnce, false, message.clone())
+        .await?;
+
+    info!("Message published. Waiting for it to be flushed...");
+    loop {
+        match event_loop.poll().await {
+            Ok(event) => match event {
+                Event::Incoming(inc_message) => {
+                    match inc_message {
+                        Incoming::Connect(_) => {}
+                        Incoming::ConnAck(_) => {}
+                        Incoming::Publish(_) => {}
+                        Incoming::PubAck(_) => {
+                            info!("Message publication acknowledged!");
+                            break;
+                        }
+                        Incoming::PubRec(_) => {}
+                        Incoming::PubRel(_) => {}
+                        Incoming::PubComp(_) => {}
+                        Incoming::Subscribe(_) => {}
+                        Incoming::SubAck(_) => {}
+                        Incoming::Unsubscribe(_) => {}
+                        Incoming::UnsubAck(_) => {}
+                        Incoming::PingReq => {}
+                        Incoming::PingResp => {}
+                        Incoming::Disconnect => {}
+                    }
+                }
+                Event::Outgoing(outgoing) => {
+                    match outgoing {
+                        Outgoing::Publish(_) => {
+                            info!("Message published. Waiting for ack...");
+                        }
+                        Outgoing::Subscribe(_) => {}
+                        Outgoing::Unsubscribe(_) => {}
+                        Outgoing::PubAck(_) => {}
+                        Outgoing::PubRec(_) => {}
+                        Outgoing::PubRel(_) => {}
+                        Outgoing::PubComp(_) => {}
+                        Outgoing::PingReq => {}
+                        Outgoing::PingResp => {}
+                        Outgoing::Disconnect => {}
+                        Outgoing::AwaitAck(_) => {}
+                    }
+                }
+            },
+            Err(e) => {
+                error!("Error = {:?}", e);
+            }
+        }
+
+        sleep(Duration::from_millis(100)).await;
+    }
+
+    Ok(())
+}
